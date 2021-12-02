1ca2fd7e6fc0da46d2b52f7576e50d9581ce02f2
vhzuniga <vhzuniga@espol.edu.ec>
2021-12-02 13:32:06 -0500

    Evaluacion e Inscripcion

--- proyectoParcial1ClasesAbrahamDanielaVictor/src/main/java/ec/edu/espol/proyectoparcial1clasesabraham/model/Evaluacion.java
+++ proyectoParcial1ClasesAbrahamDanielaVictor/src/main/java/ec/edu/espol/proyectoparcial1clasesabraham/model/Evaluacion.java
@@ -5,10 +5,97 @@
  */
 package ec.edu.espol.proyectoparcial1clasesabraham.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Abraham, Daniela, Victor
  */
 public class Evaluacion {
-    
+    private int id;
+    private Inscripcion idInscripcion;
+    private Inscripcion inscripcion;
+    private int idMiembroJurado;
+    private double nota;
+    private int idCriterio;
+    private Criterio criterio;
+
+    public Evaluacion(Inscripcion idInscripcion, int idCriterio) {
+        this.idInscripcion = idInscripcion;
+        this.idCriterio = idCriterio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Inscripcion getIdInscripcion() {
+        return idInscripcion;
+    }
+
+    public void setIdInscripcion(Inscripcion idInscripcion) {
+        this.idInscripcion = idInscripcion;
+    }
+
+    public Inscripcion getInscripcion() {
+        return inscripcion;
+    }
+
+    public void setInscripcion(Inscripcion inscripcion) {
+        this.inscripcion = inscripcion;
+    }
+
+    public int getIdMiembroJurado() {
+        return idMiembroJurado;
+    }
+
+    public void setIdMiembroJurado(int idMiembroJurado) {
+        this.idMiembroJurado = idMiembroJurado;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public int getIdCriterio() {
+        return idCriterio;
+    }
+
+    public void setIdCriterio(int idCriterio) {
+        this.idCriterio = idCriterio;
+    }
+
+    public Criterio getCriterio() {
+        return criterio;
+    }
+
+    public void setCriterio(Criterio criterio) {
+        this.criterio = criterio;
+    }
+    public void saveFile(String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true )))
+        {
+            pw.println(jurado.getEmail()+","+MiembroJurado.getEmail()+","+this.id,this.idCriterio+","+this.nota);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        }
+
+    @Override
+    public String toString() {
+        return "Evaluacion{" + "id=" + id + ", idInscripcion=" + idInscripcion + ", inscripcion=" + inscripcion + ", idMiembroJurado=" + idMiembroJurado + ", nota=" + nota + ", idCriterio=" + idCriterio + ", criterio=" + criterio + '}';
+    }
+        
 }
---
+++ proyectoParcial1ClasesAbrahamDanielaVictor/src/main/java/ec/edu/espol/proyectoparcial1clasesabraham/model/Inscripcion.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.proyectoparcial1clasesabraham.model;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Victor Zuñiga
+ */
+public class Inscripcion {
+    private int id;
+    private int idMascota;
+    private Concurso concurso;
+    private double valor;
+    private double descuento;
+    private ArrayList<Evaluacion> evaluaciones;
+    private ArrayList<Inscripcion> incripciones;
+    private Date fecha;
+    
+    public Inscripcion(Scanner String, Scanner String2){
+        
+        Scanner sc= new Scanner(System.in);
+        String nombreConcurso= sc.next();
+        System.out.println("Ingrese el valor a pagar");
+        this.valor= sc.nextDouble();
+        try {
+            
+            System.out.println("Introduzca la fecha de incripcion con formato dd/mm/yyyy");
+            SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd");
+            this.fecha= formato.parse(sc.next());
+        } catch (ParseException ex) {
+            Logger.getLogger(Inscripcion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.evaluaciones=new ArrayList<>();
+        this.incripciones=new ArrayList<>();
+    }
+    public void saveFile(String nomfile){
+        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile),true )))
+        {
+            pw.println(this.idMascota+","+concurso.getIdConcurso()+","+this.fecha+","+this.valor);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+  
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion" + this.idMascota+","+concurso.getIdConcurso()+","+this.fecha+","+this.valor ;
+    }
+    
+    
+}
